c9fc7ee07778ba6028255eadf8dec8107d3c45f3
Bari Fuchs <bari.fuchs@gmail.com>
2017-12-12 11:13:36 -0500

    commit prepro

--- prepro.py
+++ prepro.py
@@ -6,3 +6,22 @@ Created on Tue Dec 12 10:54:11 2017
 @author: barifuchs
 """
 
+import glob
+import os
+import pdb
+import subprocess
+import argparse
+import datetime
+import shutil
+
+def prepro(basedir):
+    #do something cool
+    print('Data is in path '+basedir)
+def main():
+    #load in all global variables prepro needs, right now this is the path to the data
+    basedir='Users/barifuchs/UNCworkshop_data/ds000030_R1.0.5'
+    prepro(basedir) #call prepro to do cool things
+    
+main()
+#runs main, which runs basedir (we defined basedir in main)
+#main holds global variable (basedir)
